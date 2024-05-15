14cc9371917e591654aaed82696ff2cee8d087e0
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2024-05-15 12:37:17 +0200

    fix: format to main.cpp

--- 05_collatz_feather/main.cpp
+++ 05_collatz_feather/main.cpp
@@ -12,10 +12,10 @@ int collatz(const int n) { return ((n % 2) * (2 * n + 1) + n) / 2; }
 template <typename T, typename Func>
 std::vector<double> vectTrans(const std::vector<T>& vec, Func func) {
   std::vector<double> result;
-  result.reserve( vec.size() );
+  result.reserve(vec.size());
 
   for (std::size_t i = 1; i < vec.size(); i++) {
-    result.push_back( func( (double)vec[i] ) );
+    result.push_back(func((double)vec[i]));
   }
   return result;
 }
@@ -23,9 +23,9 @@ std::vector<double> vectTrans(const std::vector<T>& vec, Func func) {
 template <typename T>
 std::vector<T> cumsum(const std::vector<T>& vec) {
   std::vector<T> result;
-  result.reserve( vec.size() );
+  result.reserve(vec.size());
 
-  result.push_back( vec[0] );
+  result.push_back(vec[0]);
   for (std::size_t i = 1; i < vec.size(); i++) {
     result.push_back(vec[i] + result[i - 1]);
   }
@@ -33,16 +33,18 @@ std::vector<T> cumsum(const std::vector<T>& vec) {
   return result;
 }
 
-std::vector<double> rescale(const std::vector<double>& vec, const double leftLimit, const double rightLimit) {
+std::vector<double> rescale(const std::vector<double>& vec,
+                            const double leftLimit, const double rightLimit) {
   std::vector<double> result;
-  result.reserve( vec.size() );
+  result.reserve(vec.size());
 
   auto min = std::min_element(vec.begin(), vec.end());
   auto max = std::max_element(vec.begin(), vec.end());
 
   double temp;
   for (std::size_t i = 0; i < vec.size(); i++) {
-    temp = leftLimit + ( (vec[i] - *min) / (*max - *min)) * (rightLimit - leftLimit);
+    temp = leftLimit +
+           ((vec[i] - *min) / (*max - *min)) * (rightLimit - leftLimit);
     result.push_back(temp);
   }
   return result;
@@ -72,25 +74,25 @@ int main(int argc, char** argv) {
   std::vector<double> t, x, y;
 
   for (int i = 0; i < distElements; i++) {
-    sequence.push_back( distrib(rng) );
+    sequence.push_back(distrib(rng));
 
     while (sequence.back() > 1) {
-      sequence.push_back( collatz( sequence.back() ) );
+      sequence.push_back(collatz(sequence.back()));
     }
 
     std::reverse(sequence.begin(), sequence.end());
 
-    auto mod = [](double x) {return std::fmod(x ,2.0); };
-    auto cos = [](double x) {return std::cos(x); };
-    auto sin = [](double x) {return std::sin(x); };
+    auto mod = [](double x) { return std::fmod(x, 2.0); };
+    auto cos = [](double x) { return std::cos(x); };
+    auto sin = [](double x) { return std::sin(x); };
 
     t.reserve(sequence.size());
     x.reserve(sequence.size());
     y.reserve(sequence.size());
 
     t = cumsum(rescale(vectTrans(sequence, mod), ratioLeft, ratioRight));
-    x = cumsum( vectTrans(t, cos) );
-    y = cumsum( vectTrans(t, sin) );
+    x = cumsum(vectTrans(t, cos));
+    y = cumsum(vectTrans(t, sin));
 
     // Set fixed-point notation and precision
     std::cout << std::fixed << std::setprecision(5);
