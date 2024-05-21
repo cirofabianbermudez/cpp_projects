266a926255956cde2752bb7e4c2778251260bd15
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2024-05-21 19:48:15 +0200

    feat: OS detection

--- 06_mandelbrot_set/main.cpp
+++ 06_mandelbrot_set/main.cpp
@@ -1,19 +1,19 @@
 #include <cmath>
+#include <complex>
 #include <iomanip>
 #include <iostream>
-#include <complex>
 
-double mandelbrot ( std::complex<double> c) {
+double mandelbrot(std::complex<double> c) {
   // Skiping first iteration 0.0, 0.0
-  std::complex<double> zn( c );
+  std::complex<double> zn(c);
   int iter = 45;
   double max_iter = (double)iter;
 
   for (int n = 0; n < iter; n++) {
-    zn = zn*zn + c;
-    //std::cout << "DEBUB" << std::endl;
-    //std::cout << std::abs(zn) << std::endl;
-    if ( std::abs(zn) > 2.0 ) {
+    zn = zn * zn + c;
+    // std::cout << "DEBUB" << std::endl;
+    // std::cout << std::abs(zn) << std::endl;
+    if (std::abs(zn) > 2.0) {
       return (double)n;
     }
   }
@@ -22,7 +22,6 @@ double mandelbrot ( std::complex<double> c) {
 }
 
 int main(int argc, char** argv) {
-
   double x0 = -0.6;
   double r = 1.2;
   double points = 1000;
@@ -31,13 +30,12 @@ int main(int argc, char** argv) {
   // Set fixed-point notation and precision
   std::cout << std::fixed << std::setprecision(5);
 
-  for (double y = -r; y < r; y += (r+r)/(points-1.0)) {
-    for (double x = x0-r; x < x0+r; x += (r+r)/(points-1.0)) {
-      c = std::complex<double>(x,y);
+  for (double y = -r; y < r; y += (r + r) / (points - 1.0)) {
+    for (double x = x0 - r; x < x0 + r; x += (r + r) / (points - 1.0)) {
+      c = std::complex<double>(x, y);
       std::cout << std::setw(8) << x << ", " 
-                << std::setw(8) << y << ", " 
-                << std::setw(8) <<  mandelbrot(c) <<
-                std::endl;
+                << std::setw(8) << y << ", "
+                << std::setw(8) << mandelbrot(c) << std::endl;
     }
     std::cout << std::endl;
   }
